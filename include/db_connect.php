b405c2e29c8ccde8b56a97a377ea849436662a7f
Naser Man <tgza54@gmail.com>
2026-05-23 02:14:53 +0300

    prepare for railway database update

--- include/db_connect.php
+++ include/db_connect.php
@@ -5,16 +5,23 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
-
 $host = getenv('DB_HOST');
+$db   = getenv('DB_DATABASE');
 $user = getenv('DB_USERNAME');
 $pass = getenv('DB_PASSWORD');
-$name = getenv('DB_DATABASE');
-$port = getenv('DB_PORT');
-$conn = new mysqli($host, $user, $pass, $name, $port);
+$port = getenv('DB_PORT') ?: 3306;
 
-if ($conn->connect_error) {
-    die("Connection Failed: " . $conn->connect_error);
+try {
+    $conn = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass
+    );
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Connection Failed: " . $e->getMessage());
 }
 // $conn = mysqli_connect('localhost','root','','Libey') or die('فشل الاتصال بقاعدة البيانات');
 // // إعدادات بايبال
