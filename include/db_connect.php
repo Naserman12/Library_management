9436c471b557a57c4473209874cacc23ca25cc3e
Naser Man <tgza54@gmail.com>
2026-05-23 00:18:33 +0300

    prepare for railway

--- include/db_connect.php
+++ include/db_connect.php
@@ -1,5 +1,18 @@
 <?php
-$conn = mysqli_connect('localhost','root','','Libey') or die('فشل الاتصال بقاعدة البيانات');
-// إعدادات بايبال
-require_once "config.php";
-?>
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$name = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'];
+
+$conn = new mysqli($host, $user, $pass, $name, $port);
+
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
