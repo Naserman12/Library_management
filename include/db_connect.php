2313a6a730457f7a655cc95bd8a03679d0cf9185
Naser Man <tgza54@gmail.com>
2026-05-24 21:57:28 +0300

    Routes gegenv()

--- include/db_connect.php
+++ include/db_connect.php
@@ -3,11 +3,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-$host = $_ENV['DB_HOST'];
-$db   = $_ENV['DB_DATABASE'];
-$user = $_ENV['DB_USERNAME'];
-$pass = $_ENV['DB_PASSWORD'];
-$port = $_ENV['DB_PORT'] ?: 3306;
+$host = getenv('DB_HOST');
+$db   = getenv('DB_DATABASE');
+$user = getenv('DB_USERNAME');
+$pass = getenv('DB_PASSWORD');
+$port = getenv('DB_PORT') ?: 3306;
 
 try {
     $conn = new PDO(
