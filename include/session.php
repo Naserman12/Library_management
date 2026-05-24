f21ffd2f8960836238c11855d2d1182f2c30da59
Naser Man <tgza54@gmail.com>
2026-05-24 22:57:19 +0300

    Routes gegenv()

--- include/session.php
+++ include/session.php
@@ -1,12 +1,13 @@
 <?php
 
 ini_set('session.use_strict_mode', 1);
+
 session_set_cookie_params([
     'lifetime' => 0,
     'path' => '/',
-    'secure' => true, // Railway أحياناً يحتاج false
+    'secure' => true,      // Railway = HTTPS
     'httponly' => true,
-    'samesite' => 'Lax'
+    'samesite' => 'None'   // أهم شيء
 ]);
 
- session_start();
+session_start();
